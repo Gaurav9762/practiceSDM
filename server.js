13b7be8728283795d875928da249ff9b6e832ca9
62002_Gaurav <deshpandegaurav57@gmail.com>
2023-02-03 16:30:21 +0530

    simplenode

---
+++ server.js
@@ -0,0 +1,17 @@
+var exp = require('express');
+var app = exp();
+var path = require('path');
+
+app.use(express.static(path.join(__dirname,'public')));
+
+
+var server = app.listen(9000,function(){
+
+    console.log("server created");
+
+});
+
+app.get("/",function(req,res){
+
+        res.send({fname:"Gaurav Deshpande"});
+});
